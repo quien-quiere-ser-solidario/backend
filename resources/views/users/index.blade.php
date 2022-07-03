ebddf3d0b88367409d538c3fe377614fa7ad7ffd
Adrián Pelayo <niniohhns@gmail.com>
2022-07-04 00:29:45 +0200

    started users index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-@foreach ($users as $user)
-<p>{{$user->username}}</p>
-@endforeach
+<div class="h-full flex flex-col justify-between items-center gap-5 py-20">
+    <h1 class="font-k2d text-3xl font-black uppercase">Usuarios</h1>
+    <div class="w-full h-full overflow-y-auto scroll-smooth my-2 flex flex-col items-center">
+        @foreach ($users as $user)
+        <p>{{$user->username}}</p>
+        @endforeach
+    </div>
 </div>
 @endsection
